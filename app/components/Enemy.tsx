7a65f78f9316446c756143785fd8d3069aeb3a82
7S-Codere <dylan.e33@hotmail.fr>
2025-12-12 07:01:11 +0100

    patch Enemy

--- app/components/Enemy.tsx
+++ app/components/Enemy.tsx
@@ -12,11 +12,8 @@ type Props = {
 };
 
 export default function Enemy({ id, name, rarity, level, x, y, hp }: Props) {
-	const cls = rarity ? `enemy-name ${rarity}` : "enemy-name";
-	return (
-		<div className="enemy" data-id={id} style={{ position: "absolute", left: x, top: y }}>
-			<div className={cls}>{name ?? id}{level ? ` (lvl ${level})` : ''}</div>
-			<div>HP: {hp}</div>
-		</div>
+	return ( 
+    <>
+    </>
 	);
 }
